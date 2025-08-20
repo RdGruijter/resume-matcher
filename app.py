52e2a4ca0d8b8ea0c76ce6139d283cc03ecfd9be
Macbook Air van Rutger <rutgerdegruijter@MacBook-Air-van-Macbook.local>
2025-08-20 10:47:31 +0200

    Test API, change base url

--- app.py
+++ app.py
@@ -111,7 +111,9 @@ def fetch_jobs_from_api(query, country_code):
     """
     Fetches job listings from a RapidAPI Job Search API.
     """
-    base_url = f"https://{RAPIDAPI_HOST}/search"
+    base_url = f"https://{st.secrets['RAPIDAPI_HOST']}/search"
+    #f"https://{RAPIDAPI_HOST}/search"
+    
     
     headers = {
         "X-RapidAPI-Key": st.secrets['RAPIDAPI_KEY'],
